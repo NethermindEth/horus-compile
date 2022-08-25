187d8ef2e47e3ef771b82ac6a736c2d7c31e928e
Artem Yurchenko <artemyurchenko@zoho.com>
2022-08-26 00:45:03 +0600

    fix: stop swallowing errors in the unnecessary try-except block
    The try-except block just swallows information and doesn't set the
         proper exit code. It might look like it does, because of the `if
         __name__ == "__main__:…""`, but that is not executed. The
         horus-compile executable just calls the main function and not the
         file itself.
    
    To make the API more convenient to use, we inject dependency on
         arguments into main. The separate function for the purposes of
         poetry scripts is created, which passes sys.argv (without the
         script name, i.e. the first arg) to main.
    
    Additionally, non-conforming contracts (non-constant exponentiation)
         are deleted. They weren't detected before, because the error was
         swallowed.

--- tests/division/loop.cairo
+++
@@ -1,9 +0,0 @@
-# @declare $k : felt
-func main():
-    [ap] = 1; ap++
-    # @require [ap - 1] == 1
-    # @invariant $k == ap - fp && [ap - 1] == 2 ** ($k - 1)
-    loop:
-    [ap] = [ap - 1] * 2; ap++
-    jmp loop
-end
--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,18 +1,14 @@
 import glob
 import json
-import sys
 from io import StringIO
 from os.path import exists
 
-import marshmallow_dataclass
-
-from horus.compiler.contract_definition import HorusDefinition
 from horus.compiler.horus_compile import main
 
 
-def test_golden(capsys, monkeypatch):
-    def run_horus_compile():
-        main()
+def test_golden(capsys):
+    def run_horus_compile(file):
+        main([file])
         out = capsys.readouterr().out
         program_json = json.loads(out)
         with StringIO() as checks_out:
@@ -23,42 +19,21 @@ def test_golden(capsys, monkeypatch):
             json.dump(output, checks_out, indent=2, sort_keys=True)
             return checks_out.getvalue()
 
-    with monkeypatch.context() as m:
-        for file in glob.glob("./tests/golden/*.cairo"):
-            with capsys.disabled():
-                print(file)
-            gold_name = file.replace(".cairo", ".gold")
-            if not exists(gold_name):
-                m.setattr(sys, "argv", ["", file])
-                out = run_horus_compile()
-                with open(gold_name, "w") as f:
-                    f.write(out)
-            else:
-                with open(gold_name, "r") as gold:
-                    text = gold.read()
-
-                m.setattr(
-                    sys,
-                    "argv",
-                    [
-                        "",
-                        file,
-                    ],
-                )
-
-                out = run_horus_compile()
-                assert text == out
+    for file in glob.glob("./tests/golden/*.cairo"):
+        with capsys.disabled():
+            print(file)
+        gold_name = file.replace(".cairo", ".gold")
+        if not exists(gold_name):
+            out = run_horus_compile(file)
+            with open(gold_name, "w") as f:
+                f.write(out)
+        else:
+            with open(gold_name, "r") as gold:
+                text = gold.read()
+            out = run_horus_compile(file)
+            assert text == out
 
 
-def test_division(monkeypatch):
-    with monkeypatch.context() as m:
-        for file in glob.glob("./tests/division/*.cairo"):
-            m.setattr(
-                sys,
-                "argv",
-                [
-                    "",
-                    file,
-                ],
-            )
-            main()
+def test_division():
+    for file in glob.glob("./tests/division/*.cairo"):
+        main([file])
