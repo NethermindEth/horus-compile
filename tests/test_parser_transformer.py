f1a4768244cb0331a9a5975e8f69827197611219
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-11-23 07:28:35 -0500

    Improve docstrings in `test_parser_transformer.py`.

--- tests/test_parser_transformer.py
+++ tests/test_parser_transformer.py
@@ -12,8 +12,8 @@ TESTS = Path(__file__).parent
 
 def test_parse_handles_non_annotation_code():
     """
-    Do we get an `UnexpectedToken` error when there's a logical identifier
-    outside an annotation?
+    Test that `UnexpectedToken` errors are not raised when there's a logical
+    identifier outside an annotation.
     """
     code = (TESTS / "logical_identifier_outside_annotation.cairo").read_text()
     with pytest.raises(ParserError):
@@ -21,5 +21,6 @@ def test_parse_handles_non_annotation_code():
 
 
 def test_z3_transformer_visit_expr_identifier():
+    """Test `visit_ExprIdentifier()` on a trivial case for sake of coverage."""
     t = Z3ExpressionTransformer()
     t.visit_ExprIdentifier(ExprIdentifier(name=""))
